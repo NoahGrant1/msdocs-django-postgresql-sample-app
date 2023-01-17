3b443ddd7d40818bce7dfa3cc10d94a83260ce5e
Noah Grant <104340106+NoahGrant1@users.noreply.github.com>
2023-01-17 13:28:13 +1100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 python manage.py migrate
-gunicorn --workers 2 --threads 4 --timeout 300 --access-logfile \
+gunicorn --workers 2 --threads 4 --timeout 3000 --access-logfile \
     '-' --error-logfile '-' --bind=0.0.0.0:8000 \
      --chdir=/home/site/wwwroot azureproject.wsgi
